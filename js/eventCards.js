6a5c6f9850beab96a80de67a137e79d7307fe6c3
EladKarni <39969481+EladKarni@users.noreply.github.com>
2019-02-13 21:42:34 -0500

    added a crude schedule feature

---
+++ js/eventCards.js
@@ -0,0 +1,25 @@
+let maxResults = 4
+let numberOfEvents = 0
+let currectDate = new Date().toISOString()
+let EventsDiv = document.getElementById('EventCards')
+
+$(document).ready(function() {
+  console.log(
+    GSL[0].time.toString().substring(0, GSL[i].time.toString().length - 9)
+  )
+  for (let i = 0; i < GSL.length && i < 3; i++) {
+    let startTime = GSL[i].time
+      .toString()
+      .substring(0, GSL[i].time.toString().length - 9)
+
+    let card = `
+        <li class="card">
+            <div class="card--text">
+                <div class="text--container"> 
+                    ${startTime} 
+                </div>                    
+            </div>
+        </li>`
+    EventsDiv.innerHTML += card
+  }
+})
