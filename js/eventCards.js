ffe496f8184426b7bd2b421df756122bf6edbd40
EladKarni <39969481+EladKarni@users.noreply.github.com>
2019-02-13 21:44:39 -0500

    more cards on screen for next events

--- js/eventCards.js
+++ js/eventCards.js
@@ -7,7 +7,7 @@ $(document).ready(function() {
   console.log(
     GSL[0].time.toString().substring(0, GSL[i].time.toString().length - 9)
   )
-  for (let i = 0; i < GSL.length && i < 3; i++) {
+  for (let i = 0; i < GSL.length && i < 4; i++) {
     let startTime = GSL[i].time
       .toString()
       .substring(0, GSL[i].time.toString().length - 9)
