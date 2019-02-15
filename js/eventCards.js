c2abc93d34104020cdbd75b7a992887d0107fb88
EladKarni <39969481+EladKarni@users.noreply.github.com>
2019-02-15 06:01:21 -0500

    only show future events in cards

--- js/eventCards.js
+++ js/eventCards.js
@@ -1,25 +1,34 @@
-let maxResults = 4
+let maxResults = 3
 let numberOfEvents = 0
 let currectDate = new Date().toISOString()
 let EventsDiv = document.getElementById('EventCards')
+const TODAY = moment()
 
 $(document).ready(function() {
-  console.log(
-    GSL[0].time.toString().substring(0, GSL[i].time.toString().length - 9)
-  )
-  for (let i = 0; i < GSL.length && i < 4; i++) {
-    let startTime = GSL[i].time
-      .toString()
-      .substring(0, GSL[i].time.toString().length - 9)
+  let currentEvents = 0
+  for (let i = 0; i < GSL.length && currentEvents < maxResults; i++) {
+    if (0 > TODAY.diff(GSL[i].time)) {
+      console.log(TODAY.diff(GSL[i].time))
+      let startDate = GSL[i].time
+        .toString()
+        .substring(0, GSL[i].time.toString().length - 18)
+      let startTime = GSL[i].time
+        .toString()
+        .substring(15, GSL[i].time.toString().length - 9)
 
-    let card = `
+      let card = `
         <li class="card">
             <div class="card--text">
                 <div class="text--container"> 
-                    ${startTime} 
+                    <span class="startDate">${startDate}</span>
+                    <br>
+                    <span class="startTime">${startTime}</span>
                 </div>                    
             </div>
         </li>`
-    EventsDiv.innerHTML += card
+      EventsDiv.innerHTML += card
+      console.log(i)
+      currentEvents++
+    }
   }
 })
